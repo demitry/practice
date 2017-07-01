2c3fa4e59b3091fbc3588dd273208cb4f5526f76
DESKTOP-FCMOFM8\Dmitry <aurorabitbucket@gmail.com>
2017-07-01 14:53:07 +0300

    client

--- WCF/WCFMyServiceLibrary/WindowsFormsApplication/Form1.Designer.cs
+++ WCF/WCFMyServiceLibrary/WindowsFormsApplication/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(884, 355);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "btn_Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(93, 12);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(75, 23);
+            this.btn_Send.TabIndex = 3;
+            this.btn_Send.Text = "btn_Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(174, 12);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.btn_Close.TabIndex = 4;
+            this.btn_Close.Text = "btn_Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 442);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button btn_Close;
     }
 }
 
--- WCF/WCFMyServiceLibrary/WindowsFormsApplication/Form1.cs
+++ WCF/WCFMyServiceLibrary/WindowsFormsApplication/Form1.cs
@@ -1,20 +1,137 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
 using System.Windows.Forms;
+using ServiceReference1;
 
 namespace WindowsFormsApplication
 {
+
     public partial class Form1 : Form
     {
+
+        MyServiceClient client = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Print(string text)
+        {
+            richTextBox1.Text += text + "\n\n";
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void Print(Exception ex)
+        {
+            if (ex == null) return;
+            Print(ex.Message);
+            Print(ex.Source);
+            Print(ex.StackTrace);
+        }
+
+        private void Create_New_Client()
+        {
+            if (client == null)
+                try { Try_To_Create_New_Client(); }
+                catch (Exception ex)
+                {
+                    Print(ex);
+                    Print(ex.InnerException);
+                    client = null;
+                }
+            else
+            {
+                Print("Cannot create a new client. The current Client is active.");
+            }
+        }
+
+        private void Try_To_Create_New_Client()
+        {
+            try
+            {
+
+                NetTcpBinding binding = new NetTcpBinding(SecurityMode.Transport);
+
+                binding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+                binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+                binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+                //string uri = "net.tcp://192.168.1.2:9002/MyService";
+                string uri = "net.tcp://localhost:9002/MyService";
+
+                EndpointAddress endpoint = new EndpointAddress(new Uri(uri));
+
+                client = new MyServiceClient(binding, endpoint);
+
+                //client.ClientCredentials.Windows.ClientCredential.Domain = "";
+                //client.ClientCredentials.Windows.ClientCredential.UserName = "Vasya Pupkin";
+                //client.ClientCredentials.Windows.ClientCredential.Password = "";
+
+                client.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+
+                Print("Creating new client ....");
+                Print(endpoint.Uri.ToString());
+                Print(uri);
+
+                string test = client.Method1("test");
+
+                if (test.Length < 1)
+                {
+                    throw new Exception("Проверка соединения не удалась");
+                }
+                else
+                {
+                    Print("test is OK  ! " + test);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Print(ex);
+                Print(ex.InnerException);
+                client = null;
+            }
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            Create_New_Client();
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            Print("sending message . . .");
+            string s = textBox1.Text;
+            string x = "";
+            if (client != null)
+            {
+                x = client.Method1(s);
+                Print(x);
+                x = client.Method2(s);
+                Print(x);
+            }
+            else
+            {
+                Print("Error! Client does not exist!");
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            if (client != null)
+            {
+                Print("Closing a client ...");
+                client.Close();
+                client = null;
+            }
+            else
+            {
+                Print("Error! Client does not exist!");
+            }
+            this.Close();
+        }
+
     }
 }
