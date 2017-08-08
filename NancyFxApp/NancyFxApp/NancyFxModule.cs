fe613001b4eddf8f03c19b4d608a8888f404f3db
Unknown <dpoluektov@gmail.com>
2017-08-08 23:14:22 +0300

    first nancy app

---
+++ NancyFxApp/NancyFxApp/NancyFxModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFxApp
+{
+    public class NancyFxModule: NancyModule
+    {
+        public NancyFxModule()
+        {
+            Get["/"] = param => View["Index.html"];
+        }
+
+    }
+}
