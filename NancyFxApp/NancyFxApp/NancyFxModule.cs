774149b5fbda9f028a3b9590abeeae13971ee06a
Unknown <dpoluektov@gmail.com>
2017-08-08 23:44:31 +0300

    nancy param and viewBag

--- NancyFxApp/NancyFxApp/NancyFxModule.cs
+++ NancyFxApp/NancyFxApp/NancyFxModule.cs
@@ -1,8 +1,4 @@
 ﻿using Nancy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace NancyFxApp
 {
@@ -11,7 +7,15 @@ namespace NancyFxApp
         public NancyFxModule()
         {
             Get["/"] = param => View["Index.html"];
-        }
 
+            Get["/{Name}"] = param =>
+            {
+                dynamic viewBag = new DynamicDictionary();
+                viewBag.Name = param.Name;
+                return View["Hello.html", viewBag];
+
+                /*В NancyFx использует по умолчанию Super Simple View Engine. В отличие от Razor она не поддерживает ViewBag и мы имитируем VieBag добавлением DinamicDictionary, с последующим добавлением оного во ViewModel. При желании можно подключить пакет Razor и пользоваться всеми преимуществами именно это привычного графического движка*/
+            };
+        }
     }
 }
